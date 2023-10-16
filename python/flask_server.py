a052a349a18f468f17fffbeadb82d2a8845c2e36
sunjiabin <sunjiabin01@outlook.com>
2023-10-16 18:56:43 +0800

    update flask server

--- python/flask_server.py
+++ python/flask_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint
 from flask import request
 import base64
+import logging
 
 import threading
 from grpc_client import get_image_classify_result
@@ -29,12 +30,31 @@ def img_classify():
     res = get_image_classify_result(address, img_path=img_path)
     return res
 
+@app.route('/img_cls_base64', methods=['POST'])
+def img_classify_base64():
+    img_base64 = request.form.get('img_base64')
+    logging.info("img_base64: {}".format(img_base64))
+    address = "localhost:50051"
+    res = get_image_classify_result(address, img_base64=img_base64)
+    return res
+
+
+
 # 定义启动 Web 服务器的函数
 def run_server(port):
     app.run(port=port)
 
 # 在 app 中注册两个 Blueprint，并启动两个 Web 服务器，分别监听 8000 和 9000 端口
 if __name__ == '__main__':
+    logging.basicConfig(
+        format='[%(asctime)s] [%(levelname)s] %(filename)s:%(lineno)d\t%(message)s', 
+        datefmt="%H:%M:%S",
+        level=logging.INFO,
+        handlers=[
+            logging.StreamHandler(),
+        ]
+    )
+
     # app.register_blueprint(bp_server, url_prefix='')
     t1 = threading.Thread(target=run_server, args=(8000,))
     t1.start()
